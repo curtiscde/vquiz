2f2731f7cc5fa9f20527f57553fb706ff5c5e06d
Curtis Timson <curt@live.co.uk>
2020-04-23 09:04:15 +0100

    Add unit tests

---
+++ functions/src/util/validators.test.js
@@ -0,0 +1,9 @@
+import validateLoginData from './validators';
+
+describe('validators', () => {
+  describe('validateLoginData', () => {
+    it('should return true', () => {
+      expect(true).toBe(true);
+    })
+  });
+});
