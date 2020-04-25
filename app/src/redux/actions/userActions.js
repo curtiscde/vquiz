5a44d47d24e844900b5c4027234374da92e98c97
Curtis Timson <curt@live.co.uk>
2020-04-25 17:42:30 +0100

    refactor userActions

--- app/src/redux/actions/userActions.js
+++ app/src/redux/actions/userActions.js
@@ -1,19 +1,15 @@
 import * as types from './actionTypes';
 import * as userApi from '../../api/userApi';
 
-function loginSuccess(token) {
-  return {
-    type: types.LOGIN_SUCCESS,
-    token,
-  };
-}
+const loginSuccess = (token) => ({
+  type: types.LOGIN_SUCCESS,
+  token,
+});
 
-function loginFailure(errors) {
-  return {
-    type: types.LOGIN_FAILURE,
-    errors,
-  };
-}
+const loginFailure = (errors) => ({
+  type: types.LOGIN_FAILURE,
+  errors,
+});
 
 // eslint-disable-next-line import/prefer-default-export
 export function login(fields) {
