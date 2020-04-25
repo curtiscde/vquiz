35089299285e0b9ed0abd4bfc1e2f1c9d94f7321
Curtis Timson <curt@live.co.uk>
2020-04-25 11:51:50 +0100

    Move eslint to specific file

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,4 +1,33 @@
 import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: 10
+  },
+  progess: {
+    position: 'absolute'
+  }
+});
 
 const Login = () => (
   <div>
@@ -6,4 +35,4 @@ const Login = () => (
   </div>
 );
 
-export default Login;
+export default withStyles(styles)(Login);
