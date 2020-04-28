15c4c01b7a83dedf4ff2fd1fe9012f47fe563152
Curtis Timson <curt@live.co.uk>
2020-04-28 20:54:25 +0100

    Update QuizzesList UI

--- app/src/components/QuizzesList.js
+++ app/src/components/QuizzesList.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 const QuizzesList = ({ quizzes }) => (
-  quizzes.map((quiz) => (
-    <Card key={quiz.quizId}>
-      <CardContent>
-        {quiz.title}
-      </CardContent>
-    </Card>
-  ))
+  <Grid container spacing={3}>
+    {
+      quizzes.map((quiz) => (
+        <Grid key={quiz.quizId} item xs={12} sm={6}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="h2">{quiz.title}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))
+    }
+  </Grid>
 );
 
 QuizzesList.propTypes = {
