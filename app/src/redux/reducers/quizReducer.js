b60f4a24b7d7d46da66f467e4a615c5f907e9c0a
Curtis Timson <curt@live.co.uk>
2020-04-30 18:45:58 +0100

    Remove redundant reducer case

--- app/src/redux/reducers/quizReducer.js
+++ app/src/redux/reducers/quizReducer.js
@@ -3,11 +3,6 @@ import initialState from './initialState';
 
 export default function quizReducer(state = initialState.quiz, action) {
   switch (action.type) {
-    case types.CREATE_QUIZ_PENDING:
-      return {
-        ...state,
-        fetching: true,
-      };
     case types.FETCH_QUIZ_PENDING:
       return {
         ...state,
