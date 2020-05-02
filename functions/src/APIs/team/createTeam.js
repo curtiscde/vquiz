296b46dbfafebbdd1d0d9deb580172a11d332239
Curtis Timson <curt@live.co.uk>
2020-05-02 16:21:08 +0100

    Add team to subcollection

--- functions/src/APIs/team/createTeam.js
+++ functions/src/APIs/team/createTeam.js
@@ -1,20 +1,45 @@
+import firebase from 'firebase';
+import { db } from '../../util/admin';
 import { isEmpty } from '../../util/validators';
 
 export function createTeam(req, res) {
-  if (isEmpty(req.body.quizId)) {
+  const { quizId, name } = req.body;
+
+  if (isEmpty(quizId)) {
     return res.status(400).json({ quizId: 'Must not be empty' });
   }
 
-  if (isEmpty(req.body.name)) {
+  if (isEmpty(name)) {
     return res.status(400).json({ name: 'Must not be empty' });
   }
 
   const newTeam = {
-    name: req.body.name,
+    createdAt: new Date().toISOString(),
+    name,
   };
 
+  try {
+    const teams = db
+      .collection(`quiz`)
+      .doc(quizId)
+      .collection('team');
 
+    return teams
+      .where('name', '==', newTeam.name)
+      .get()
+      .then((snapshot) => {
+        if (!snapshot.empty) {
+          return res.status(400).json({ name: 'Already exists ' });
+        }
+        return teams
+          .add(newTeam)
+      })
+      .then((team) => {
+        newTeam.id = team.id;
+        res.status(200).json(newTeam);
+      });
 
-
-  return res.status(200).json({ ok: true });
+  } catch (error) {
+    return res.status(500).send();
+  }
 }
