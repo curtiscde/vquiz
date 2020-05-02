ef56d549f591fd0f61213f32bb4529097613ab18
Curtis Timson <curt@live.co.uk>
2020-05-02 19:31:47 +0100

    Add removeImage route for teams

---
+++ functions/src/APIs/team/removeImage.js
@@ -0,0 +1,35 @@
+import firebaseAdmin from 'firebase-admin';
+import { db, admin } from '../../util/admin';
+import { isEmpty } from '../../util/validators';
+
+function deleteImage(imageName) {
+  const bucket = admin.storage().bucket();
+  return bucket.file(imageName).delete()
+    .then(() => ({}))
+    .catch(() => ({}));
+}
+
+export default function removeImage(req, res) {
+  const { quizId, teamId } = req.body;
+  const { FieldValue } = firebaseAdmin.firestore.FieldValue;
+
+  if (isEmpty(quizId)) {
+    return res.status(400).json({ quizId: 'Must not be empty' });
+  }
+  if (isEmpty(teamId)) {
+    return res.status(400).json({ teamId: 'Must not be empty' });
+  }
+
+  const doc = db.doc(`quiz/${quizId}/team/${teamId}`);
+
+  return doc
+    .get()
+    .then((snapshot) => {
+      const { imageFileName } = snapshot.data();
+      deleteImage(`team/${imageFileName}`);
+      return doc.update({
+        imageFileName: null,
+      });
+    })
+    .then(() => res.send());
+}
