a5600e1f4ea549528a0dd63f88c8bbb1c85e654b
Curtis Timson <curt@live.co.uk>
2020-05-03 18:19:32 +0100

    get image download url

--- functions/src/APIs/team/getTeams.js
+++ functions/src/APIs/team/getTeams.js
@@ -1,4 +1,28 @@
-import { db } from '../../util/admin';
+import { db, admin } from '../../util/admin';
+
+function getTeamFromDoc(doc) {
+  return {
+    teamId: doc.id,
+    name: doc.data().name,
+    createdAt: doc.data().createdAt,
+    imageFileName: doc.data().imageFileName,
+  };
+}
+
+async function getImageDownloadUrl(imageFileName) {
+  if (!imageFileName) {
+    return null;
+  }
+
+  const file = admin.storage().bucket().file(`team/${imageFileName}`);
+
+  return file.getSignedUrl({
+    action: 'read',
+    expires: new Date().getTime() + (60 * 60 * 1000),
+  })
+    .then((signedUrls) => signedUrls[0])
+    .catch(() => null);
+}
 
 export default function (req, res) {
   const { quizId } = req.params;
@@ -11,15 +35,15 @@ export default function (req, res) {
       .get()
       .then((snapshot) => {
         const teams = [];
-        snapshot.forEach((doc) => {
-          teams.push({
-            teamId: doc.id,
-            name: doc.data().name,
-            createdAt: doc.data().createdAt,
-            imageUrl: doc.data().imageUrl ? doc.data().imageUrl : null,
-          });
-        });
-        return res.status(200).json(teams);
+        snapshot.forEach((doc) => teams.push(getTeamFromDoc(doc)));
+        return teams;
+      })
+      .then(async (teams) => {
+        const t = await Promise.all(teams.map(async (team) => ({
+          ...team,
+          imageUrl: await getImageDownloadUrl(team.imageFileName),
+        })));
+        return res.status(200).json(t);
       });
   } catch (error) {
     return res.status(500).send();
