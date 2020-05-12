46f708718373151830e43a01b0df2e381a0c7450
Curtis Timson <curt@live.co.uk>
2020-05-12 20:17:43 +0100

    Add totalScore

--- functions/src/APIs/score/submitScore.js
+++ functions/src/APIs/score/submitScore.js
@@ -1,5 +1,45 @@
 import { db } from '../../util/admin';
 import { isEmpty } from '../../util/validators';
+import groupScoresByTeam from './util/aggregationHelper';
+
+function getScoresByTeam(quizId) {
+  return db
+    .collection('quiz')
+    .doc(quizId)
+    .collection('score')
+    .get()
+    .then((snapshot) => {
+      const scores = [];
+      snapshot.forEach((doc) => {
+        scores.push({
+          docId: doc.id,
+          score: doc.data().score,
+        });
+      });
+      return groupScoresByTeam(scores);
+    })
+}
+
+function aggregateScores(quizId, teamScores) {
+  return db
+    .collection('quiz')
+    .doc(quizId)
+    .collection('team')
+    .get()
+    .then((snapshot) => {
+      const teams = [];
+      snapshot.forEach((doc) => {
+        db.collection('quiz')
+          .doc(quizId)
+          .collection('team')
+          .doc(doc.id)
+          .update({
+            totalScore: teamScores[doc.id] || 0,
+          });
+      });
+      return teams;
+    })
+}
 
 export default function (req, res) {
   const { quizId, scores } = req.body;
@@ -26,6 +66,8 @@ export default function (req, res) {
     });
 
     return batch.commit()
+      .then(() => getScoresByTeam(quizId))
+      .then((teamScores) => aggregateScores(quizId, teamScores))
       .then(() => res.send());
   } catch (error) {
     // eslint-disable-next-line no-console
