13fc85ea6a415384e8569d45b76b7361d2ca63a3
Curtis Timson <curt@live.co.uk>
2020-05-14 20:43:14 +0100

    Remove redundant express app

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,4 +1,3 @@
-import express from 'express';
 import firebase from 'firebase';
 import config from './util/config';
 import authApp from './APIs/auth';
@@ -8,17 +7,10 @@ import roundApp from './APIs/round';
 import teamApp from './APIs/team';
 import scoreApp from './APIs/score';
 
-import cors from './middleware/cors';
-
 const functions = require('firebase-functions');
-
-const app = express();
 
 firebase.initializeApp(config.firebase);
 
-app.use(cors);
-
-exports.api = functions.region('europe-west1').https.onRequest(app);
 exports.auth = functions.region('europe-west1').https.onRequest(authApp);
 exports.user = functions.region('europe-west1').https.onRequest(userApp);
 exports.quiz = functions.region('europe-west1').https.onRequest(quizApp);
